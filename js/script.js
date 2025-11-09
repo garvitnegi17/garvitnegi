25cb293d0acd665347b872e004dd1d72257dbbd9
Garvit Negi <garvitnegi537@gmail.com>
2025-11-09 14:23:17 +0530

    Fixed icon

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ const folderIcon = document.getElementById('folderIcon');
 folderIcon.addEventListener('click', () => {
   folderIcon.classList.toggle('active');
 });
+
+folderIcon.addEventListener('touchstart', () => {
+    folderIcon.classList.add('hover-mobile'); 
+});
+
+folderIcon.addEventListener('touchend', () => {
+    folderIcon.classList.remove('hover-mobile'); 
+});
